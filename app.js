4ae77ac26ac48f22a20f0244a4468ccacb8fe761
kabir <kabir.kakkar4@gmail.com>
2020-08-04 18:13:30 +0530

    Added Post Request for Registration Form

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ app.use (require("express-session")({
     resave: false,
     saveUninitialized: false
 }));
+//Including body-parser so we can access the data in the form using req.body.(name)
+app.use (bodyParser.urlencoded({extended: true}));
 
 // They are responsible for reading the session, taking the data from the session,
 // Encode it - SerailizeUser
@@ -30,12 +32,45 @@ app.use (require("express-session")({
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser (User.deserializeUser());
 
+// ======================
+// ROUTES
+// ======================
+
 app.get("/", function(req, res){
     res.render("home.ejs");
 });
 
 app.get("/secret", function(req, res){
     res.render("secret.ejs");
+});
+
+// AUTH ROUTES
+// show sign up form
+app.get("/register", function(req, res){
+    res.render("register.ejs");
+});
+
+//handling user signup
+app.post ("/register", function(req, res){
+    // We only pass in username but not the password
+    // Instead we add the password as the second parameter
+    // User.register takes the password(the second argument) and hashes it 
+    // then finally the call back function takes the new user and puts everything inside of it.
+    // It has username and the hash password as well
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if (err) {
+            console.log (err);
+            return res.render('register');
+        } 
+        // Once the user has been created and there is not an error
+        // We run passport.authenticate which will log the user in, run the passport serializeUser method, store 
+        // information and specify that we want to use the "local" strategy (it can be "twitter" if we want to sign up with twitter but we use local here as we want the information to be stored locally)
+        
+        passport.authenticate("local")(req, res, function(){
+            // Once the user has ben logged in, we redirect to the secret page
+            res.redirect("/secret");
+        });
+    });
 });
 
 app.listen(3000, function(){
