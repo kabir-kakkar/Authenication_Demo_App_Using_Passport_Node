a8f5f7665ba6edf255372ef1f898e203987a956d
kabir <kabir.kakkar4@gmail.com>
2020-08-04 12:34:21 +0530

    Configured Passport

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var express = require("express"),
     passport = require('passport'),
     bodyParser = require('body-parser'),
     LocalStrategy = require('passport-local'),
-    passportLocalMongoose = require('passport-local-mongoose');
+    passportLocalMongoose = require('passport-local-mongoose'),
+    User = require ('./models/user');
 
 mongoose.connect('mongodb://localhost:27017/auth_demo_app', {
   useNewUrlParser: true,
@@ -14,6 +15,20 @@ mongoose.connect('mongodb://localhost:27017/auth_demo_app', {
 .catch(error => console.log(error.message));
 
 app.set ('view engine', 'ejs');
+// Initializing passport session
+app.use (passport.initialize());
+app.use (passport.session());
+app.use (require("express-session")({
+    secret: "Wingardium Leviosa",
+    resave: false,
+    saveUninitialized: false
+}));
+
+// They are responsible for reading the session, taking the data from the session,
+// Encode it - SerailizeUser
+// And Decode it - DeserializeUser
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser (User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("home.ejs");
